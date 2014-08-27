99bb4d3420227c299edef6b04cf58312507fa156
Marco de Jongh <marco.dejongh@gfk.com>
2014-08-27 17:12:43 +0200

    More boilerplate

--- application/application.js
+++ application/application.js
@@ -2,7 +2,11 @@ if (Meteor.isClient) {
   Template.hello.events({
     'click button': function (e, template) {
       // increment the counter when button is clicked
-      Awesome.addMessage(template.find('input').value);
+      if (Awesome) {
+				Awesome.addMessage(template.find('input').value);
+			} else {
+				console.log('Awesome to be implemented')
+			}
     }
   });
 }
